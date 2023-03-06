e31d675bc0f038d7faff5d5160b8493f195ee9c2
graymastermind <82908514+graymastermind@users.noreply.github.com>
2023-03-07 00:47:54 +0200

    updates

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Question 1 Assignment 1'
 
 
 if __name__ == '__main__':
